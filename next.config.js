5d5df73f888f19cd7def88f309b2926a5754f3e7
Emperor Biggy <rifelinktechnology@gmail.com>
2025-04-28 08:24:37 +0100

    new changes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    reactStrictMode: true,
-    images: {
-      domains: ['stackfood.6am.one'],
-      staticPageGenerationTimeout: 1500, // Increase timeout to 5 minutes
-    },
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        config.watchOptions = {
-          aggregateTimeout: 300, // Delay the rebuild after changes
-          poll: 1000, // Check for changes every second
-        };
-      }
-      return config;
-    },
-  }
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['stackfood.6am.one'],
+    staticPageGenerationTimeout: 600, // Moderate timeout for static page generation (10 minutes)
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.watchOptions = {
+        aggregateTimeout: 300, // Delay the rebuild after changes
+        poll: process.env.NODE_ENV === 'development' ? 1000 : false, // Only poll in development
+      };
+    }
+    return config;
+  },
+}
